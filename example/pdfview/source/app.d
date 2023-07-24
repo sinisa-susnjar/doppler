8e32fa4fc0a173bdd4a43686ad914aad25186f25
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2023-07-24 04:46:43 +0200

    added key events

--- example/pdfview/source/app.d
+++ example/pdfview/source/app.d
@@ -8,6 +8,8 @@ import gtk.DrawingArea;
 import gtk.Main;
 import gtk.Box;
 import gtk.Widget;
+import gdk.Event;
+import gdk.Keysyms;
 import cairo.Context;
 import cairo.ImageSurface;
 import cairo.Surface;
@@ -79,19 +81,51 @@ class AppBox : Box {
 
 class MyDrawingArea : DrawingArea {
 	immutable int xOffset = 0, yOffset = 0;
+    Document document;
 	Surface surface;
+    int pageNo = 0;
+    int nPages;
     Page page;
 
 	this(Document doc) {
-        auto nPages = doc.getNPages();
-        page = doc.getPage(0);
+        document = doc;
+        nPages = document.getNPages();
+        page = document.getPage(pageNo);
         int width, height;
         page.getSize(width, height);
         surface = ImageSurface.create(CairoFormat.ARGB32, width, height);
 		addOnDraw(&onDraw);
+
+        setCanFocus(true);
+        setEvents(EventMask.KEY_PRESS_MASK);
+        addOnKeyPress((Event e, Widget widget) {
+            uint keyVal;
+            e.getKeyval(keyVal);
+            final switch (keyVal) {
+                case Keysyms.GDK_Escape:
+                    writefln("Bye!");
+                    Main.quit();
+                    break;
+                case Keysyms.GDK_Right:
+                    if (pageNo < nPages-1) {
+                        pageNo++;
+                        page = document.getPage(pageNo);
+                        queueDraw();
+                    }
+                    break;
+                case Keysyms.GDK_Left:
+                    if (pageNo > 0) {
+                        pageNo--;
+                        page = document.getPage(pageNo);
+                        queueDraw();
+                    }
+                    break;
+            }
+            return true;
+        });
 	}
 
-	bool onDraw(Scoped!Context context, Widget w) {
+	bool onDraw(Scoped!Context context, Widget) {
         context.setSourceSurface(surface, xOffset, yOffset);
         page.render(context);
 		context.paint();
